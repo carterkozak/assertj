85036976f59a384a58d4eda88e88cd16eb562efb
Carter Kozak <ckozak@ckozak.net>
2019-09-18 12:22:30 -0400

    Fix #1607: Improve HamcrestCondition generic type inference

--- src/test/java/org/assertj/core/api/HamcrestConditionTest.java
+++ src/test/java/org/assertj/core/api/HamcrestConditionTest.java
@@ -14,8 +14,12 @@ package org.assertj.core.api;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.core.StringContains.containsString;
+import static org.hamcrest.Matchers.empty;
 
 import org.junit.jupiter.api.Test;
+
+import java.util.Collection;
+import java.util.Collections;
 
 public class HamcrestConditionTest {
 
@@ -24,8 +28,18 @@ public class HamcrestConditionTest {
     Condition<String> aStringContainingA = new HamcrestCondition<>(containsString("a"));
 
     assertThat("abc").is(aStringContainingA)
-                     .has(aStringContainingA)
-                     .satisfies(aStringContainingA);
+                            .has(aStringContainingA)
+                            .satisfies(aStringContainingA);
     assertThat("bc").isNot(aStringContainingA);
+  }
+
+  @Test
+  public void should_be_able_to_use_a_hamcrest_matcher_with_generic() {
+    Collection<? extends CharSequence> emptyIterable = Collections.emptyList();
+    Collection<? extends CharSequence> oneElementIterable = Collections.singletonList("item");
+    assertThat(emptyIterable).is(new HamcrestCondition<>(empty()))
+                             .has(new HamcrestCondition<>(empty()))
+                             .satisfies(new HamcrestCondition<>(empty()));
+    assertThat(oneElementIterable).isNot(new HamcrestCondition<>(empty()));
   }
 }
